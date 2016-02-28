5207216c420f3e3fdb533d8051814f5f402dc3d5
Shangguan <sgcy1991@qq.com>
2016-02-28 17:26:51 +0800

    update

--- trade.py
+++ trade.py
@@ -4,11 +4,11 @@ import math
 import tushare as ts
 from ggplot import *
 
-def calculateHold(df,date,stocks_num):
+def calculateHold(df,date):
     total_df = df[df.index == date]
     total_df = total_df.T
     total_df = total_df.sort([date])
-    holds = list(total_df.index)[:stocks_num]
+    holds = list(total_df.index)[:30]
     return holds
 
 def getPrice(price_df,stock,date):
@@ -35,26 +35,6 @@ def getTradePrice(price_df,stock,date,isBuy):
         else:
             return price
 
-
-def shouldBuy(price_df,sh,stock,date):
-    # return True
-    sh_index = list(sh.index).index(date)
-    last_sh_index = sh_index
-
-    if last_sh_index < 0:
-        return True
-    sh_change = sh.iloc[sh_index]["close"]/sh.iloc[last_sh_index]["close"] - 1 
-
-    price_change = price_df.iloc[sh_index][stock]/price_df.iloc[last_sh_index][stock] - 1
-    if math.isnan(price_change):
-        return True
-
-    change_delta = price_change - sh_change
-    if sh_change == 0:
-        return True
-    return change_delta/abs(sh_change) < -0.5
-    # return change_delta < -0.1
-
 def notST(st_df,date):
     def make_filter(stock):
         if not stock in st_df.columns:
@@ -70,9 +50,39 @@ def notST(st_df,date):
                 return False
     return make_filter
 
-TRADE_DETAIL_LOG = True
+def getRecentPrices(price_df,stock,date,days):
+    df = price_df[price_df.index <= date]
+    prices = list(df[stock])
+    return prices[len(prices)-days:len(prices)]
 
-def trade(stocks_num,stop_line):
+def should_hold(price_df,date):
+    def make_filter(stock):
+        try:
+            prices = getRecentPrices(price_df,stock,date,4)
+            if prices[3]/prices[0] < 0.92 or prices[3]/prices[1] < 0.95:
+                #print "[DELETE]:" + stock
+                return False
+            return True
+        except Exception, e:
+            return True
+    return make_filter
+
+def not_new_stock(price_df,date):
+    def make_filter(stock):
+        if date < "2013-02-18":
+            return True
+        try:
+            prices = getRecentPrices(price_df,stock,date,4)
+            if prices[3]/prices[2] > 1.098 and prices[2]/prices[1] > 1.098 and prices[1]/prices[0] > 1.098:
+                return False
+            return True
+        except Exception, e:
+            return False
+    return make_filter
+
+TRADE_DETAIL_LOG = False
+
+def trade(stocks_num):
 
     total_df = pd.read_csv('total.csv',index_col = 0)
 
@@ -114,17 +124,21 @@ def trade(stocks_num,stop_line):
             print total_asset
 
         #计算持仓
-        hold = calculateHold(total_df,date,stocks_num)
-        not_st = notST(st_df,date)
-        hold = filter(not_st, hold)
-        hold_set = set(hold)
+        hold = calculateHold(total_df,date)
 
-        date_index = list(sh.index).index(date)
-        if date_index > 0:
-            last_index = date_index - 1
-            last_close = sh.iloc[last_index]["close"]
-            if row['close']/last_close < 1 - stop_line:
-                hold_set = set([])
+        hold = hold[:stocks_num]
+
+        not_st_filter = notST(st_df,date)
+        hold = list(filter(not_st_filter, hold))
+
+        # not_new_filter = not_new_stock(price_df,date)
+        # hold = list(filter(not_new_filter, hold))
+
+
+        # should_hold_filter = should_hold(price_df,date)
+        # hold = list(filter(should_hold_filter, hold))
+
+        hold_set = set(hold)
         
         to_sell = current_hold - hold_set
         to_buy = hold_set - current_hold
@@ -144,7 +158,6 @@ def trade(stocks_num,stop_line):
             if TRADE_DETAIL_LOG:
                 print "[SELL]" + stock + "--" + str(price) + "--" + str(amount) +  "--" + str(money)
 
-
         if len(to_buy) == 0:
             current_hold = hold_set
             continue
@@ -153,16 +166,23 @@ def trade(stocks_num,stop_line):
         # should_not_buy = set([])
         # for stock in to_buy:
         #     price = getTradePrice(price_df,stock,date,True)
-        #     if math.isnan(price) or not shouldBuy(fill_price_df,sh,stock,date):
-        #         hold_set.remove(stock)
+        #     if math.isnan(price):
+        #         try:
+        #             hold_set.remove(stock)
+        #         except Exception, e:
+        #             print hold_set
         #         should_not_buy.add(stock)
 
         # to_buy = to_buy - should_not_buy
 
         each = money/len(to_buy)
+
+        # if each > total_asset/(stocks_num):
+        #     each = total_asset/(stocks_num)
+
         for stock in to_buy:
             price = getTradePrice(price_df,stock,date,True)
-            if each < 0.00001 or math.isnan(price):
+            if each < 0.00001 or math.isnan(price) :
                 hold_set.remove(stock)
                 continue
             hold_amount[stock] = each/price
@@ -176,7 +196,7 @@ def trade(stocks_num,stop_line):
             print hold_amount
 
     #股数－收益
-    #return list(my_values)[-1]
+    return list(my_values)[-1]
 
     #股数－回撤
     # max_redraw = 0
@@ -191,29 +211,30 @@ def trade(stocks_num,stop_line):
     # return max_redraw
 
     #绘制
-    df['my_value'] = my_values
-    df['index_value'] = index_values
+    # df['my_value'] = my_values
+    # df['index_value'] = index_values
+    # df = df.reset_index()
+    # df = df.fillna(method="ffill",axis=0)
+    # df['date'] = pd.to_datetime(df['date'])
+    # df = pd.melt(df,id_vars = ["date"],value_vars = ['my_value','index_value']) 
+    # plot = ggplot(df,aes(x = "date", y = "value",color = "variable")) + geom_line()
+    # print plot
+
+
+def calculate_stocks_num():
+    from_to = range(3,30)
+    se = pd.Series(index=from_to)
+    for i in from_to:
+        value = trade(i)
+        print value
+        se[i] = value
+    df = pd.DataFrame(se)
     df = df.reset_index()
-    df = df.fillna(method="ffill",axis=0)
-    df['date'] = pd.to_datetime(df['date'])
-    df = pd.melt(df,id_vars = ["date"],value_vars = ['my_value','index_value']) 
-    plot = ggplot(df,aes(x = "date", y = "value",color = "variable")) + geom_line()
+    df.columns = ["stocks_num","value"]
+    print df
+    df.to_csv('redraw_result.csv')
+    plot = ggplot(df,aes(x = "stocks_num", y = "value")) + geom_line()
     print plot
 
-
-# def calculate_stocks_num():
-#     from_to = range(10,70)
-#     se = pd.Series(index=from_to)
-#     for i in from_to:
-#         value = trade(8,float(i)/1000)
-#         se[i] = value
-#     df = pd.DataFrame(se)
-#     df = df.reset_index()
-#     df.columns = ["stop_line","redraw"]
-#     print df
-#     df.to_csv('redraw_result.csv')
-#     plot = ggplot(df,aes(x = "stop_line", y = "redraw")) + geom_line()
-#     print plot
-
-# calculate_stocks_num()
-trade(8,0.03)
+calculate_stocks_num()
+#trade(8)
