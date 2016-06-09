9cf2249962fa9504a5fc811277594955996024ee
xdbaqiao <yexj061761@gmail>
2016-06-09 10:11:21 +0000

    delete volume temp directory

--- getVolume.py
+++ getVolume.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
+# !/usr/bin/python2
+# coding:utf-8
 
-# import sys
-# reload(sys) 
-# sys.setdefaultencoding("utf8")
-# sys.path.append('/Users/sgcy/anaconda/lib/python2.7/site-packages')
-
-from bs4 import BeautifulSoup
 import requests 
 import os
 import string
@@ -13,91 +8,84 @@ import urllib2
 import csv
 import pandas as pd
 import tushare as ts
-        
-def getStocks():
-    stocks_df = ts.get_stock_basics()
-    stocks = list(stocks_df.index)
-    return stocks
+from collections import deque
+from bs4 import BeautifulSoup
 
-html_page_base = "http://f10.eastmoney.com/f10_v2/CapitalStockStructure.aspx?code="
-def getData(stock):
-    number = int(stock)
-    stock_name = ""
-    if number > 500000:
-        stock_name = "sh" + stock
-    else:
-        stock_name = "sz" + stock
-    url = html_page_base + stock_name
-    if number > 300280:
-        fetch_data_url(stock,url)
+DATA_SOURCE = 'sina'
 
+class download:
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0'
+        self.headers = {'User-Agent': self.user_agent, 'Accept-encoding':'gzip, deflate'}
+        self.opener = urllib2.build_opener()
+
+    def get(self, url):
+        request = urllib2.Request(url)
+        response = self.opener.open(request)
+        html = response.read()
+        return html
 
 def extract_text(node):
     return node.get_text()
 
-def fetch_data_url(code,url):
-    header ={'User-Agent':'mozilla/5.0 (windows; U; windows NT 5.1; zh-cn)'}
-    req = urllib2.Request(url,None,header)
-    response = urllib2.urlopen(req)
-    page = response.read()
-    soup = BeautifulSoup(page)
-    table = soup.find("table",id = "lngbbd_Table")
-    file_name = "volumes/" + code + ".csv"
-    with open(file_name, 'wb') as fp:
-        csv_writer = csv.writer(fp,delimiter=',')
-
-        #添加表内容
-        trs = table.find_all('tr')[:2]
-        for tr in trs:
-            ths = tr.find_all('th')
-            if len(ths) == 1:
-                ths = tr.find_all('td')
-            else:
-                ths = ths[2:]
-            texts = map(extract_text,ths)
-            csv_writer.writerow(texts)
-
-def fetch_data_url2(code,url):
-    header ={'User-Agent':'mozilla/5.0 (windows; U; windows NT 5.1; zh-cn)'}
-    req = urllib2.Request(url,None,header)
-    response = urllib2.urlopen(req)
-    page = response.read()
-    soup = BeautifulSoup(page,"html5lib")
-    table = soup.find("table",id = "StockStructureHistoryTable")
-    table2 = soup.find_all("table")
-    file_name = "volumes/" + code + ".csv"
-    with open(file_name, 'wb') as fp:
-        csv_writer = csv.writer(fp,delimiter=',')
-        trs = table.find_all('tr')
-        for tr in trs:
+def clean_data(html):
+    results = []
+    soup = BeautifulSoup(html,"html5lib") if DATA_SOURCE == 'sina' else BeautifulSoup(page)
+    table = soup.find("table",id = "StockStructureHistoryTable") if DATA_SOURCE == 'sina' else\
+            soup.find("table",id = "lngbbd_Table")
+    trs = table.find_all('tr') if DATA_SOURCE == 'sina' else table.find_all('tr')[:2]
+    for tr in trs:
+        if DATA_SOURCE == 'sina':
             tds = tr.find_all('td')
             if len(tds) == 2:
                 texts = map(extract_text,tds)
-                csv_writer.writerow(texts)
+                results.append(texts)
+        else:
+            ths = tr.find_all('th')
+            tds = tr.find_all('td') if len(ths) == 1 else ths = ths[2:]
+            texts = map(extract_text,tds)
+            results.append(texts)
+    return results
 
-def getData2(stock):
-    number = int(stock)
-    if number > 0:
-        url = "http://money.finance.sina.com.cn/corp/go.php/vCI_StockStructureHistory/stockid/" + stock + "/stocktype/TotalStock.phtml"
-        fetch_data_url2(stock,url)
+def get_volume_data(urls):
+    volume_infos = []
+    def worker():
+        D = download()
+        while True:
+            try:
+                url = urls.popleft()
+            except IndexError:
+                break
+            print 'Downloading: %s' % url
+            html = D.get(url=url)
+            volume_infos = clean_data(html)
+
+    muilt_thread(worker, 30)
+    return volume_infos
+
+def muilt_thread(target, num_threads, wait=True):
+    threads = [threading.Thread(target=target) for i in range(num_threads)]
+    for thread in threads:
+        thread.start()
+    if wait:
+        for thread in threads:
+            thread.join()
+
+def getStocks():
+    stocks_df = ts.get_stock_basics()
+    stocks = list(stocks_df.index)
+    return stocks
         
 def update_volume():
-    errors = []
-    for stock in getStocks():
-        try:
-            getData2(stock)
-        except Exception as e:
-            print("[Error]:" + stock)
-            print(e)
-            errors.append(stock)
+    url = 'http://money.finance.sina.com.cn/corp/go.php/vCI_StockStructureHistory/stockid/%s/stocktype/TotalStock.phtml'\
+            if DATA_SOURCE=='sina' else 'http://f10.eastmoney.com/f10_v2/CapitalStockStructure.aspx?code=%s'
+    urls = deque()
+    for i in getStocks():
+        int_stock = int(i)
+        if DATA_SOURCE != 'sina':
+            i = 'sh%s' % i if int_stock > 500000 else 'sz%s' % i
+        urls.append(url % i)
+    get_volume_data(urls)
 
-    print("-----")
-    for stock in errors:
-        try:
-            getData2(stock)
-            print("[fix]:" + stock)
-        except Exception as e:
-            print(stock)
-            print(e)
-
-# go()
+if __name__ == '__main__':
+    update_volume()
