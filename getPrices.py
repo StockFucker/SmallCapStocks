7afcc2add590e6a03731f0e4e9cbdba3bdd2e15e
xdbaqiao <yexj061761@gmail>
2016-06-10 09:11:29 +0000

    getPrices: add mutil-threading, speed up!

--- getPrices.py
+++ getPrices.py
@@ -1,28 +1,48 @@
-# -*- coding: utf-8 -*-
+# !/usr/bin/python2
+# coding: utf-8
+
+import threading
 import tushare as ts
 import pandas as pd
+from common import get_stock_code
+from collections import deque
 
-def getStocks():
-    stocks_df = ts.get_stock_basics()
-    stocks = list(stocks_df.index)
-    return stocks
+def get_price(): 
+    stocks = deque()
+    for stock in get_stock_code():
+        stocks.append(stock)
 
-def tryGetData(stock):
-    try:
-        df = ts.get_hist_data(stock) #两个日期之间的前复权数据
-        return df 
-    except Exception, e:
-        print "[Error]:" + stock
-        return tryGetData(stock)
+    prices = []
+    def worker():
+        while True:
+            try:
+                stock = stocks.popleft()
+            except IndexError:
+                break
+            print 'Get Price data from tushare: %s' % stock
+            try:
+                df = ts.get_hist_data(stock) #两个日期之间的前复权数据
+            except Exception:
+                stocks.append(stock)
+                continue
+            df['stock_id'] = stock
+            prices.append(df)
+    muilt_thread(worker, 30)
+    write_2_csv(prices)
 
-def getData(stock):
-    df = tryGetData(stock)
-    file_name = "prices/" + stock + ".csv"
-    if df is not None:
-        df.to_csv(file_name)
-    else:
-        print "[Miss]:" + stock
+def muilt_thread(target, num_threads, wait=True):
+    threads = [threading.Thread(target=target) for i in range(num_threads)]
+    for thread in threads:
+        thread.start()
+    if wait:
+        for thread in threads:
+            thread.join()
 
-def go(): 
-    for stock in getStocks():
-        getData(stock)
+def write_2_csv(prices):
+    file_name = 'csv/prices.csv'
+    result = reduce(lambda x,y:x+y, prices)
+    if result is not None:
+        result.to_csv(file_name)
+
+if __name__ == '__main__':
+    get_price()
