1f76eb0e0ed3262c3a52a8d9b3da9364dce8b72e
xdbaqiao <yexj061761@gmail>
2016-06-15 10:37:44 +0000

    little modify

--- smallcapstock.py
+++ smallcapstock.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python2
+#coding: utf-8 
 
 import sys
 # sys.path.append('/Users/sgcy/anaconda/lib/python2.7/site-packages')
@@ -218,4 +219,5 @@ def scheduleTask():
     t = Timer(secs, calculateDailyTotalValue)
     t.start()
 
-calculateDailyTotalValue()
+if __name__ == '__main__':
+    calculateDailyTotalValue()
